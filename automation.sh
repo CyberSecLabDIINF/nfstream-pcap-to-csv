b8fdf5c75b5783e8a64a948eb75874e46340c8d2
Andres Zelaya <andres.zelaya@usach.cl>
2024-11-05 23:32:51 -0300

    Implementacion de variables dinamicas

--- automation.sh
+++ automation.sh
@@ -2,6 +2,12 @@
 
 # Uso individual: bash automation.sh -p (path to PCAPs folder) -d
 
+# Definir variables de ruta configurables
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"  # Directorio donde se encuentra el script
+VENV_DIR="$SCRIPT_DIR/venv"                                 # Ruta al entorno virtual
+PCAPS_FOLDER_DEFAULT="$SCRIPT_DI/PCAPs"                     # Carpeta predeterminada de archivos PCAP
+CSVS_FOLDER_DEFAULT="$SCRIPT_DIR/CSVs"                      # Carpeta predeterminada para archivos CSV
+
 # Function to show help
 show_help() {
     echo "Usage: $0 [option...]"
@@ -22,58 +28,35 @@ show_error() {
 # Function to activate virtual environment
 activate_virtual_environment() {
     echo "Activating the virtual environment"
-    # Temporary save the current directory
-    current_dir=$(pwd)
-    # Cd into the directory of the script if not already in it
-    if [ "$(basename "$(pwd)")" != "NFSTREAM_pre-processing" ]; then
-        cd "$(dirname "$0")" || show_error "Failed to change directory"
-    fi
-    # Check if the virtual environment directory is found
-    if [ ! -d "venv" ]; then
+    # Verificar si el directorio del entorno virtual existe
+    if [ ! -d "$VENV_DIR" ]; then
         show_error "Virtual environment directory not found"
     fi
-    # Activate the virtual environment
-    source venv/bin/activate || show_error "Failed to activate virtual environment"
-    # Check if the virtual environment is activated
+    # Activar el entorno virtual
+    source "$VENV_DIR/bin/activate" || show_error "Failed to activate virtual environment"
+    # Verificar si el entorno virtual fue activado
     if [ -n "$VIRTUAL_ENV" ]; then
         echo "Virtual environment activated: $VIRTUAL_ENV"
     else
         show_error "Failed to activate virtual environment"
     fi
-    # Cd back to the original directory
-    cd "$current_dir" || show_error "Failed to change directory"
 }
 
 # Function to install dependencies
 install_dependencies() {
     echo "Installing python dependencies"
-    # Temporary save the current directory
-    current_dir=$(pwd)
-    # Cd into the directory of the script if not already in it
-    if [ "$(basename "$(pwd)")" != "NFSTREAM_pre-processing" ]; then
-        cd "$(dirname "$0")" || show_error "Failed to change directory"
+    # Crear el entorno virtual si no existe
+    if [ ! -d "$VENV_DIR" ]; then
+        python3 -m venv "$VENV_DIR" || show_error "Failed to create virtual environment"
     fi
-    # Create a virtual environment
-    python3 -m venv venv || show_error "Failed to create virtual environment"
-    # Check if the virtual environment directory is found
-    if [ ! -d "venv" ]; then
-        show_error "Virtual environment directory not found"
-    fi
-    # Activate the virtual environment
+    # Activar el entorno virtual y proceder con la instalación
     activate_virtual_environment
-    # If in the virtual environment, install the dependencies
-    if [ -n "$VIRTUAL_ENV" ]; then
-        pip3 install -r requirements.txt || show_error "Failed to install dependencies"
-    else
-        show_error "Failed to activate virtual environment"
-    fi
-    # Deactivate the virtual environment
+    pip3 install -r "$SCRIPT_DIR/requirements.txt" || show_error "Failed to install dependencies"
+    # Desactivar el entorno virtual después de la instalación
     deactivate
-    # Cd back to the original directory
-    cd "$current_dir" || show_error "Failed to change directory"
 }
 
-# 
+# Function to process PCAP files
 process_files() {
     # Encuentra recursivamente todos los archivos .pcap dentro de la carpeta de origen
     find "$1" -type f -name "*.pcap" | while read -r file; do
@@ -83,82 +66,69 @@ process_files() {
 
 # Action to be performed on each file
 action() {
-    file=$1
-    pcaps_folder=$2
+    local file="$1"                    # Archivo actual a procesar
+    local pcaps_folder="$2"             # Carpeta donde se encuentran los archivos PCAP
     echo "Processing file: $file"
     
-    # Remove /mnt prefix from the file path for the target directory
+    # Eliminar el prefijo /mnt de la ruta del archivo para la ruta de destino
     relative_path="${file#*/mnt/}"
     relative_dir=$(dirname "$relative_path")
     name=$(basename "$file" .pcap)
 
-    # Build the target directory path omitting /mnt
+    # Construir la ruta de directorio objetivo omitiendo /mnt
     target_dir="$csvs_folder/$relative_dir"
     mkdir -p "$target_dir" || show_error "Failed to create directory structure in CSVs"
 
-    # Process the file and generate the CSV in the appropriate subfolder structure
-    if [ "$(basename "$(pwd)")" = "NFSTREAM_pre-processing" ]; then
-        python3 nfs-preprocesser.py -i "$file" -o "$target_dir/$name.csv"
-    else
-        python3 nfs-preprocesser.py -i "$file" -o "$target_dir/$name.csv"
-    fi
+    # Procesar el archivo y generar el CSV en la subcarpeta apropiada
+    python3 "$SCRIPT_DIR/nfs-preprocesser.py" -i "$file" -o "$target_dir/$name.csv"
 }
 
 # -------------------------------------- SCRIPT --------------------------------------
 
-# Check if the number of arguments is zero
+# Verificar si el número de argumentos es cero
 if [ $# -eq 0 ]; then
     show_help
 fi
 
-# Default values
-pcaps_folder="PCAPs"
-csvs_folder="/mnt/p/Programacion/USACH/Investigacion-Redes/nfstream-pcap-to-csv/CSVs"
+# Valores por defecto
+pcaps_folder="$PCAPS_FOLDER_DEFAULT"
+csvs_folder="$CSVS_FOLDER_DEFAULT"
 install_deps=false
-file_extension=".pcap"
 
-# Parse the command line arguments
+# Parsear los argumentos de la línea de comandos
 while getopts ":hdp:" opt; do
     case ${opt} in
-        h )
-            show_help
-            ;;
-        d )
-            install_deps=true
-            ;;
-        p )
-            pcaps_folder=$OPTARG
-            ;;
-        \? )
-            show_help
-            ;;
+        h ) show_help ;;                  # Mostrar ayuda
+        d ) install_deps=true ;;          # Activar instalación de dependencias
+        p ) pcaps_folder=$OPTARG ;;       # Especificar carpeta de archivos PCAP
+        \? ) show_help ;;                 # Mostrar ayuda para opciones inválidas
     esac
 done
 
-# Check if the dependencies need to be installed
+# Verificar si se deben instalar las dependencias
 if [ "$install_deps" = true ]; then
     install_dependencies
 fi
 
-# Check if the PCAPs folder exists
+# Verificar si existe la carpeta de archivos PCAP
 if [ ! -d "$pcaps_folder" ]; then
     show_error "PCAPs folder not found"
 fi
 
-# Check if the CSVs folder exists
+# Verificar si existe la carpeta de archivos CSV, si no, crearla
 if [ ! -d "$csvs_folder" ]; then
     mkdir "$csvs_folder" || show_error "Failed to create CSVs folder"
 fi
 
-# Activate the virtual environment
+# Activar el entorno virtual
 activate_virtual_environment
 
-# Process each file in the PCAPs folder
+# Procesar cada archivo en la carpeta de archivos PCAP
 process_files "$pcaps_folder"
 
-# Deactivate the virtual environment
+# Desactivar el entorno virtual
 deactivate
 
-# End of script
+# Fin del script
 echo "Done"
 exit 0
